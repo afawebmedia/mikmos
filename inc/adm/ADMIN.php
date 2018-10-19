1ea675daade779dd7a34c87514408a3ca4c68e2a
AFAwebmedia <afawebmedia@gmail.com>
2018-10-19 14:50:35 +0700

    Add files via upload

---
+++ inc/adm/ADMIN.php
@@ -0,0 +1,10 @@
+
+<?php
+/** 
+Yedin Abu Shafa 
+Kontak WA: 081802161315
+**/
+$_LEVEL = "ADMIN";
+$_USER = "demo";
+$_PASS = "Mikmos_nJafpw==";
+?>
---
+++ inc/ip_mk/EYTALASE.php
@@ -0,0 +1,16 @@
+<?php
+/** 
+Yedin Abu Shafa 
+Kontak WA: 081802161315
+**/
+$_ROUTER = "EYTALASE";
+$_IPMK = "192.168.1.100";
+$_USMK = "demo";
+$_PSMK = "Mikmos_nJafpw==";
+$_RPER = "Eytalase";
+$_RKOT = "Banten";
+$_RTEL = "08181818118";
+$_RDNS = "ey.net";
+$_RETR = "1";
+$_RLOG = "EYTALASE_EYTALASE_logo.png";
+?>
